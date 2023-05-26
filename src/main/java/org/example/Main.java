16fec51296aea6ce8a24c0420234d46990af6a26
Alexis Torres <220240@fppiramide.com>
2023-05-26 09:18:41 +0200

    Updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,25 @@
 package org.example;
 
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class Main extends JFrame {
 
-    public static String [] Columnas={"a","b","c"};
-    public static Object [][] Informacion;
+    public static String[] Columnas = {"a", "b", "c"};
+    public static Object[][] Informacion;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        SwingUtilities.invokeLater(() -> {
+            Main frame = new Main(); // Crear instancia de Main en lugar de JFrame
 
-        frame.setTitle("Archivo");
-        frame.setSize(500,320);
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new FlowLayout());
-        frame.setResizable(false);
+            frame.setTitle("Archivo");
+            frame.setSize(500, 320);
+            frame.setVisible(true);
+            frame.setLocationRelativeTo(null);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(false);
 
-        {
             Informacion = new Object[][]{
                     {"Dato 1A", "Dato 1B", "Dato 1C"},
                     {"Dato 2A", "Dato 2B", "Dato 2C"},
@@ -44,21 +42,31 @@ public class Main extends JFrame {
                     {"Dato 19A", "Dato 19B", "Dato 19C"},
                     {"Dato 20A", "Dato 20B", "Dato 20C"}
             };
-        }
 
-        String nameMenu = "Menu Archivo";
-        String[] opciones = {"Insertar", "Listar"};
-        ActionListener[] actions = new ActionListener[2];
-        actions[0] = e -> {frame.dispose();};
-        actions[1] = e -> {frame.dispose();};
+            String nameMenu = "Menu Archivo";
+            String[] opciones = {"Lista Datos", "Listar"};
+            ActionListener[] actions = new ActionListener[2];
 
-        JScrollPane ScrollPane = NewScrollPanel.NewScrollPanel(Columnas,Informacion,450,300);
+            actions[1] = e -> {
+                frame.dispose();
+            };
 
-        frame.add(ScrollPane);
+            final JScrollPane scrollPane = NewScrollPanel.NewScrollPanel(Columnas, Informacion, 450, 300);
+            actions[0] = e -> {
+                frame.panelChanger(scrollPane); // Llamar al método panelChanger en la instancia de Main
+            };
 
-        frame.setJMenuBar(NewMenu.NewMenu(nameMenu, opciones, actions));
+            frame.setJMenuBar(NewMenu.NewMenu(nameMenu, opciones, actions));
 
-        JFrameSettings.MainSettings(frame,"Archivo",500,360);
+            JFrameSettings.MainSettings(frame, "Archivo", 500, 360);
+        });
     }
 
+    public void panelChanger(Component Panel) {
+        getContentPane().removeAll();
+        getContentPane().add(Panel);
+
+        revalidate();
+        repaint();
+    }
 }
